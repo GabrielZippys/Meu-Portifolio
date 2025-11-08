ffd30744307232c53c50d75f6b09e73c73654868
Gabriel Henrique Lopes de Oliveira <gabriel.henrique@bravo-ti.com>
2025-11-08 16:55:28 -0300

    image embeeded v2

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -35,12 +35,10 @@ export const PROJECTS: Project[] = [
     links: { github: 'https://github.com/seu-usuario/agendador-bravo' },
   },
   {
-    slug: 'embeeded',
-    title: 'DashBoard Embeeded',
-    summary:
-      'Permitir que dashboards do Power BI sejam embutidos (embedded) dentro do sistema web da empresa de forma segura, respeitando as regras de acesso (RLS – Row-Level Security) conforme o tipo de usuário (administrador, colaborador etc).',
-    cover: '/images/projects/embeeded/cover.jpg',
-    tags: ['PowerBI','Next.js', 'Firebase', 'React', 'Backend/API', 'Banco de Dados', 'Power BI Embed'],
-    links: { github: 'https://github.com/seu-usuario/acougue-mais' },
+    slug: 'dashboard-embedded',
+    title: 'DashBoard Embeded',
+    summary: 'Permitir que dashboards do Power BI sejam embutidos...',
+    cover: '/images/projects/dashboard-embedded/cover.jpg', // 👈 confere esse caminho
+    tags: ['PowerBI', 'Next.js', 'Firebase', 'Backend/API', 'Banco de Dados'],
   },
 ];
